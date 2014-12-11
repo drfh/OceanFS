413429a49d44a409728532aa1c1a7e6a2fc5deeb
David RF Harris <DavidRFHarris@gmail.com>
2014-12-10 20:01:51 -0500

    Initial Commit

---
+++ OceanFS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OceanFS
+//
+//  Created by David Harris on 12/10/14.
+//  Copyright (c) 2014 MegaApps.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
