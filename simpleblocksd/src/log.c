a58e7fb3beebff1d90f8d9b366bf6e317c179679
David R. F. Harris <DavidRFHarris@gmail.com>
2019-03-11 12:16:49 -0400

    simpleblocksd - add log.c/h template

---
+++ simpleblocksd/src/log.c
@@ -0,0 +1,17 @@
+//
+//  log.c
+//  simpleblocksd
+//
+//  Created by David Harris on 3/11/2019.
+//  Copyright (c) 2018 MegaApps. All rights reserved.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sysexits.h>
+#include <sys/types.h>
+
+#include "log.h"
---
+++ simpleblocksd/src/log.h
@@ -0,0 +1,19 @@
+//
+//  log.h
+//  simpleblocksd
+//
+//  Created by David Harris on 3/11/2019.
+//  Copyright (c) 2019 MegaApps. All rights reserved.
+//
+
+#ifndef simpleblocksd_log_h
+#define simpleblocksd_log_h
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
