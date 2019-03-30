6ec39a573f2341f97f6efdd13c4abcac80328d24
David R. F. Harris <DavidRFHarris@gmail.com>
2019-03-30 12:04:25 -0400

    adding test program template and cluster config mockup

---
+++ tests/sbd_test1.c
@@ -0,0 +1,29 @@
+//
+//  test_template.c
+//  tests/template
+//
+//  Created by David Harris on 12/20/2019.
+//  Copyright (c) 2019 MegaApps. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+
+
+int main(int argc,char const **argv)
+{
+	stor_t		*d;
+	char*		name=malloc(128);
+	int			return_val=0;
+	ipc_t		ctx;
+
+	fprintf(stderr,"-----------------\n");
+	fprintf(stderr,"test (%s) - v0.1\n",__FILE__);
+	fprintf(stderr,"-----------------\n");
+
+	return 0;
+}
